0a2f1d1216b4227455b01fa17b2966f5cf742559
Carla Machado Paiano <machadogcarla@gmail.com>
2026-01-24 15:54:30 -0300

    Alteração para que urgentCount seja um computed e exiba dirto no template

--- src/app/core/urgent-tasks-badge/urgent-tasks-badge.ts
+++ src/app/core/urgent-tasks-badge/urgent-tasks-badge.ts
@@ -1,18 +1,20 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { BadgeModule } from 'primeng/badge';
 import { TaskStateService } from '../../services/task-state';
 import { OverlayBadgeModule } from 'primeng/overlaybadge';
 
 @Component({
   selector: 'app-urgent-tasks-badge',
-  imports: [BadgeModule,OverlayBadgeModule],
+  imports: [BadgeModule, OverlayBadgeModule],
   templateUrl: './urgent-tasks-badge.html',
   styleUrl: './urgent-tasks-badge.css',
 })
 export class UrgentTasksBadgeComponent {
   public taskState = inject(TaskStateService);
 
-  get urgentCount(): number {
-    return this.taskState.tasks().filter(task => task.prioridade === 'urgente' && task.status != 'concluido').length;
-  }
+  public urgentCount = computed(() => {
+    return this.taskState
+      .tasks()
+      .filter((t) => t.prioridade === 'urgente' && t.status !== 'concluido').length;
+  });
 }
